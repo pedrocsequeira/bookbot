14667fdce5b2e3520e3e6a90cbf8e76cce737224
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-19 19:12:54 +0100

    refactored count_words function; main function updated - Lesson 5 Module 1

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
 
-def count_words(book_text):
-    return len(book_text.split())
-
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(book_text)
+    num_words = get_num_words(book_text)
     print(f'{num_words} words found in the document')
 
 if __name__ == "__main__":
