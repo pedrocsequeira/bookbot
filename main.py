99d99852f74226d0d7b2f2337a99669c418fef76
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-19 19:09:42 +0100

    count_words function created; main function updated - Lesson 4 Module 1

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
 
+def count_words(book_text):
+    return len(book_text.split())
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f'{num_words} words found in the document')
 
 if __name__ == "__main__":
     main()
