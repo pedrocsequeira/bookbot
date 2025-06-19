70eb874945f1abdb44c4d4f1a669790b9f58aa3d
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-19 19:29:15 +0100

    added get_count_char function to stats.py; main function updated - Lesson 6  Module 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_count_char 
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -6,8 +6,12 @@ def get_book_text(file_path):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+    
     num_words = get_num_words(book_text)
+    char_count = get_count_char(book_text)
+    
     print(f'{num_words} words found in the document')
+    print(char_count)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+def get_count_char(book_text):
+    char_count = {}
+    for char in book_text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1 
+    return char_count
