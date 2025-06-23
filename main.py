9867ca7ef898f6f9f9973509a8d7c7d35856bc89
pedrocsequeira <pedro.c.sequeira10@gmail.com>
2025-06-23 13:20:51 +0100

    main fuction generalized and made to work with sys arguments; added exceptions when file is not found and when sargs are not in the expected format - Lesson 2  Module 2

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
+import os
+import sys
 from stats import get_num_words, get_count_char, sort_char
 
 def get_book_text(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
     with open(file_path) as file:
         return file.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     
     num_words = get_num_words(book_text)
     char_count = get_count_char(book_text)
     sorted_char_count = sort_char(char_count)
-    #print(f'{num_words} words found in the document')
-    #print(sort_char(char_count))
 
-    print(f'''============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count -----------
-Found {num_words} total words
---------- Character Count --------''')
+    print(f'''============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count -----------\nFound {num_words} total words\n--------- Character Count --------''')
     for item in sorted_char_count:
-        print(item['char'],':', item['num'])
+        print(f'{item['char']}: {item['num']}')
 
     print('============= END ===============')
 
